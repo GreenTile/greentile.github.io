f02ad2ca9b8a5803e6e6ebc987be8db4b9d160c0
Mohammad MDSA <samimd.77@gmail.com>
2019-01-13 14:59:19 +0330

    Fixed head elements

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'GreenTile',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

fbc0d2326c05872973cc6353a7d8f3a5075a6e94
Mohammad MDSA <samimd.77@gmail.com>
2019-02-02 16:32:15 +0330

    Fixed error page location

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <gt-error v-if="error.statusCode === 404" :input="errorData">
+        </gt-error>
+    </div>
+</template>
+
+<script>
+import gtError from "~/components/gt-error"
+export default {
+    components : {
+        gtError
+    },
+    props: [
+        'error'
+    ],
+    data: function() {
+        return {
+            errorData: {
+                title: ':(',
+                message: 'Unfortunately.. We couldn\'t find that page',
+                img:'/notFound.png'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
